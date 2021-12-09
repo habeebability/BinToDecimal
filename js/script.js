285f749b415c928e7940014ece51aaf6c09fe4ce
Habeebability <awoyemi.habeeb@gmail.com>
2021-12-09 12:54:45 +0100

    added boostrap for stylying

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const decimal = document.querySelector('#decimal');
 const bin = document.querySelector('#bin');
+const alertMessage = document.querySelector('#alert-text')
 
 const bin2DecBtn = document.querySelector('#bin2dec-btn');
 const dec2BinBtn = document.querySelector('#dec2bin-btn');
@@ -14,10 +15,16 @@ const convertBinToDec = () => {
     let binDigit = bin.value;
     let binArr = Array.from(binDigit);
 
+    if(binDigit.trim() === ''){
+        alert('enter a number');
+        
+    }
+
     binArr.forEach((val)=>{
-        if(val > 1) {
+        if(val > 1 ) {
             alert('Enter Combinations of zeros(0)s and one(1)')
         }
+        
         else {
             decValue.innerHTML = parseInt(binDigit,2);
         }
@@ -30,7 +37,15 @@ const convertBinToDec = () => {
 
 const convertDecToBin = () => {
     let decDigit = parseInt(decimal.value);
-    binValue.innerHTML = decDigit.toString(2);
+    if(Number.isNaN(decDigit)){
+        alert('enter a number')
+    }
+
+    
+    else {
+        binValue.innerHTML = decDigit.toString(2);
+
+    }
     
     decimal.value = '';
 
